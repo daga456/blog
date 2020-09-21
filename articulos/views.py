9743cc312a4f6d2e244ddfbd64176b6638dffafc
Diego Paredes <diegoparedes2@outlook.es>
2020-09-21 11:55:11 -0600

    separa el diseño css para el diseño bonito

--- articulos/views.py
+++ articulos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
 
@@ -9,3 +9,7 @@ def publicacion_lista(request):
         fecha_publicacion__lte=timezone.now()
     ).order_by('fecha_publicacion')
     return render(request, 'articulos/publicacion_lista.html', {"publicaciones":publicaciones})
+
+def publicacion_detalle(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk= pk)
+    return render(request, 'articulos/publicacion_detalle.html', {'publicacion': publicacion})
