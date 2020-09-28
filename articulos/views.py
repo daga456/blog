00841a4c4d3a8284215bf322ea9087a802faace0
Diego Paredes <diegoparedes2@outlook.es>
2020-09-28 13:18:02 -0600

    agregar form para insertar, editar

--- articulos/views.py
+++ articulos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
+from .formulario import PublicacionForm
 
 # Create your views here.
 
@@ -13,3 +14,30 @@ def publicacion_lista(request):
 def publicacion_detalle(request, pk):
     publicacion = get_object_or_404(Publicacion, pk= pk)
     return render(request, 'articulos/publicacion_detalle.html', {'publicacion': publicacion})
+
+def publicacion_nuevo(request):
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.fecha_creacion = timezone.now()
+            publicacion.save()
+            return redirect('publicacion_detalle', pk=publicacion.pk)
+    else:
+        formulario = PublicacionForm()
+        return render(request, 'articulos/publicacion_editar.html', {'formulario': formulario})
+
+def publicacion_editar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST, instance=publicacion)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.autor = request.user
+            publicacion.fecha_creacion = timezone.now()
+            publicacion.save()
+            return redirect('publicacion_detalle', pk=publicacion.pk)
+    else:
+        formulario = PublicacionForm(instance=publicacion)
+    return render(request, 'articulos/publicacion_editar.html', {'formulario': formulario})
