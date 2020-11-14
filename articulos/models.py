5c216f83c9717b3b57e887016b315c9c5ee3b97a
daga456 <diegoparedes2@outlook.es>
2020-11-14 16:34:45 -0600

    Prueba 6

--- articulos/models.py
+++ articulos/models.py
@@ -5,7 +5,7 @@ class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     textp = models.TextField()
-    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_creacion = models.DateTimeField('Creado',default=timezone.now)
     fecha_publicacion = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
